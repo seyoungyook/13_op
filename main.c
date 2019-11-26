aa7d2d5806c23a003beaa782cbcb5cc1bf5b4803
seyoungyook <xqd1219@naver.com>
2019-11-26 15:23:05 +0900

    practice1

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+struct student {
+	int ID;
+	char name[10];
+	double grade;
+};
+int main(void) {
+	
+	struct student s1 = {19, "seyoung", 4.3
+	};
+	return 0;
+}
